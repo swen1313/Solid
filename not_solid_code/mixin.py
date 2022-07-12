19aeb48702b0451b378fb03b4834bcc9bb11695a
swen1313 <89362389+swen1313@users.noreply.github.com>
2022-07-12 21:08:26 +0300

    Create mixin.py

---
+++ not_solid_code/mixin.py
@@ -0,0 +1,19 @@
+class PunchMixin:
+    def punch(self):
+        print('PUNCH!')
+
+
+class GunMixin:
+    def fire_a_gun(self):
+        print('PIU PIU')
+
+
+class LaserMixin:
+    def incinerate_with_lasers(self):
+        print('Wzzzuuuup!')
+
+
+class KickMixin:
+    def roundhouse_kick(self):
+        print('Bump')
+            
