99e4f93a5ce534a2bbf55c996dd20e73b3f901fd
swen1313 <89362389+swen1313@users.noreply.github.com>
2022-07-12 21:14:15 +0300

    Update heroes.py

--- not_solid_code/heroes.py
+++ not_solid_code/heroes.py
@@ -3,6 +3,19 @@ from abc import ABC, abstractmethod
 from antagonistfinder import AntagonistFinder
 
 
+
+class TV:
+    def create_news(self: 'SuperHero', place):
+        place_name = getattr(place, 'name', 'place')
+        print(f'{self.name} saved the {place_name}!')
+
+    def create_news_for_planets(self: 'SuperHero', place):
+        coordinate_list = getattr(place, 'coordinates', [])
+        if coordinate_list:
+            for coordinate in coordinate_list:
+                print(f'Planet with coordinates {coordinate} was saved by {self..name}')
+
+
 class SuperHero(ABC):
 
     def __init__(self, name, can_use_ultimate_attack=True):
