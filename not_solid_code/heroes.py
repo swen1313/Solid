61187e341ff2f3669424fc6203b46f1a68d5b356
swen1313 <89362389+swen1313@users.noreply.github.com>
2022-07-12 21:07:29 +0300

    Update heroes.py

--- not_solid_code/heroes.py
+++ not_solid_code/heroes.py
@@ -1,7 +1,9 @@
+from mixin import *
+from abc import ABC, abstractmethod
 from antagonistfinder import AntagonistFinder
 
 
-class SuperHero:
+class SuperHero(ABC):
 
     def __init__(self, name, can_use_ultimate_attack=True):
         self.name = name
@@ -11,48 +13,35 @@ class SuperHero:
     def find(self, place):
         self.finder.get_antagonist(place)
 
-    # Проблема: Герой не должен заниматься оповещениями о своей победе, это задача масс-медиа.
-    # Несоблюден: Принцип единой ответственности.
-    # По SOLID: Вынести оповещение в отдельный класс, занимающийся выводом информации.
-    # Когда возникнут трудности? Добавьте оповещение о победе героя через газеты или через TV (на выбор)
-    # а также попробуйте оповестить планеты (у которых вместа атрибута name:str используется coordinates:List[float]).
-    def create_news(self, place):
-        place_name = getattr(place, 'name', 'place')
-        print(f'{self.name} saved the {place_name}!')
-
-    # Проблема: Для каждого супергероя реализованы все методы обращения с оружием.
-    # Несоблюден: Принцип разделения интерфейса
-    # По SOLID: Создать классы-миксины для каждого оружия
-    # Когда возникнут трудности? Попробуйте запретить Чаку норрису пользоваться лазерами из глаз!
-    def fire_a_gun(self):
-        print('PIU PIU')
-
-    def incinerate_with_lasers(self):
-        print('Wzzzuuuup!')
-
-    def roundhouse_kick(self):
-        print('Bump')
-
+    @abstractmethod
     def attack(self):
-        self.fire_a_gun()
+        pass
 
-    # Проблема: У разных супергероев разные суперспособности
-    # Несоблюден: Принцип открытости/закрытости
-    # По SOLID: Каждого супергероя реализовать как наследника SuperHero и вместо изменения базового класса переопределять нужные методы
-    # Когда возникнут трудности? Когда в вашем коде поселится вся команда Мстителей
+    @abstractmethod
     def ultimate(self):
-        if self.name == 'Clark Kent':
-            self.incinerate_with_lasers()
+        pass
 
 
-class Superman(SuperHero):
+class Superman(LaserMixin, PunchMixin, SuperHero):
 
     def __init__(self):
-        super(Superman, self).__init__('Clark Kent', True)
+        super().__init__('Clark Kent', True)
 
-    # Проблема: Сигнатура метода изменилась. Если мэр города обратится к супермену как к супергерою у Кларка возникнут проблемы с атакой
-    # Несоблюден: Принцип подстановки Барбары Лисков
-    # По SOLID: Не допускать таких вольностей
-    # Когда возникнут трудности? При первой же битве
     def attack(self):
-        return 'Kick'
+        self.punch()
+
+    def ultimate(self):
+        self.incinerate_with_lasers()
+
+
+class ChuckNorris(KickMixin, GunMixin, SuperHero):
+
+    def __init__(self):
+        super().__init__('Chuck Norris', False)
+
+    def attack(self):
+        self.roundhouse_kick()
+
+    def ultimate(self):
+        self.fire_a_gun()
+
